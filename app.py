78301c7aea752ec4e79de12596f3a3bcd7136b27
Mahendra Tandon <mahendratandon.2k@gmail.com>
2023-07-16 13:26:10 +0530

    hello world

--- app.py
+++ app.py
@@ -5,6 +5,25 @@ app = Flask(__name__)
 def helloworld():
     return 'Hellow World'
 
+if __name__ == "__ORIGIN__":
+    app.run(port=3000, debug=True)
+    
+
+print ("Hello I am an awesome branch")
+print("welcome")
+
+def sum(a,b):
+    return a+b
+
+print("sum is: ",sum(2,3))
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/', method=['GET'])
+def helloworld():
+    return 'Hellow World'
+
 if __name__ == "_main_":
     app.run(port=3000, debug=True)
     
