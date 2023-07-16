ed7279fc46746e5853c99799fce8d1434e286c97
Mahendra Tandon <Mahendratandon.2k@gmail.com>
2023-07-16 13:23:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,4 +4,20 @@ def sum(a,b):
     return a+b
 print("Hello World")
 
+def sum(a,b):
+    return a+b
+print("Hello World")
+print("sum is: ",sum(2,3))
+
+def sum(a,b):
+    return a+b
+print("Hello World")
+
+print("sum is: ",sum(2,3))
+print ("Hello I am an different awesome branch")
+
+def sum(a,b):
+    return a+b
+print("Hello World")
+
 print("sum is: ",sum(2,3))
